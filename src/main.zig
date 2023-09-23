0232c9f93baad3fa6cd23c671008a088e9528c30
Tsunami Noai <spam@falseblue.com>
2023-09-22 22:42:06 -0400

    make it "modular"

--- src/main.zig
+++ src/main.zig
@@ -1,38 +1,12 @@
 const std = @import("std");
+const Day = @import("day.zig");
 
 pub fn main() !void {
     var gpa = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer gpa.deinit();
     var allocator = gpa.allocator();
 
-    const file = try std.fs.cwd().openFile("src/day1/input", .{});
-    defer file.close();
-
-    const read_buffer = try file.readToEndAlloc(allocator, 1024 * 1024);
-    defer allocator.free(read_buffer);
-
-    var it = std.mem.split(u8, read_buffer, "\n");
-
-    var elves = std.ArrayList(u32).init(allocator);
-    defer elves.deinit();
-
-    var avg: u32 = 0;
-    while (it.next()) |line| {
-        var int = std.fmt.parseInt(u32, line, 10) catch |err| {
-            if (err == error.InvalidCharacter) {
-                try elves.append(avg);
-                avg = 0;
-                continue;
-            }
-            return err;
-        };
-        avg += int;
-    }
-    var elfList = try elves.toOwnedSlice();
-    std.mem.sort(u32, elfList, {}, comptime std.sort.asc(u32));
-    var total: u32 = 0;
-    for (elfList.len - 3 .. elfList.len) |i| {
-        total += elfList[i];
-    }
-    std.debug.print("Total: {}\n", .{ total});
+    // Day 1: Calorie Counting
+    var d1 = Day.Day1.init("Day 1: Calorie Counting");
+    try d1.run(allocator);
 }
